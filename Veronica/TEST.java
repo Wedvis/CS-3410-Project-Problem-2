29faa18295e0081fa0ffcc1c4e048bdd48edaf9c
Hawkyreal <Hawkyreal@gmail.com>
2026-04-28 16:22:28 -0400

    Update TEST.java

--- Veronica/TEST.java
+++ Veronica/TEST.java
@@ -9,20 +9,28 @@ public class TEST {
 	// Protected store hashmap here
 
 	public static void main(String[] args) {
-		try {
-			readFile();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			readFile();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		AisleMap store = new AisleMap("Store", new IdTable<Item>(), new IdTable<Aisle>());
+		
 
+		ArrayList<String> PATH = new ArrayList<>();
+		PATH.add("canned");
+		
+		store.addItem(new Item(10, 10, 0, "FOOD", PATH), PATH);
 		
 
 	}
 	
 	protected static void readFile() throws FileNotFoundException {
 
-		File file1 = new File("src/group_project_p2/StoreData.txt");
+		File file1 = new File("src/group_project2/copy/TESTdata.txt");
+		AisleMap store = new AisleMap("Store", new IdTable<Item>(), new IdTable<Aisle>());
 		
 		Scanner input = new Scanner(file1);
 		String[]  temp = input.nextLine().split(":");
@@ -38,8 +46,8 @@ public class TEST {
 			if (line.equals("(start aisle)")) {
 				id = Integer.parseInt(input.nextLine().split(":")[1]);
 				aisleName = input.nextLine().split(":")[1];
-				
 				// Create an Aisle here
+				//store.getAisle(aisleName);
 				
 			}
 			if (line.equals("---")) {
@@ -47,6 +55,13 @@ public class TEST {
 				maxStock = Integer.parseInt(input.nextLine().split(":")[1]);
 				realStock = Integer.parseInt(input.nextLine().split(":")[1]);
 				String itemName = input.nextLine().split(":")[1];
+				String stringPATH = input.nextLine().split(":")[1];
+				String[] temp1 = stringPATH.split("[\\]");
+				ArrayList<String> PATH = new ArrayList<>();
+				for (String s : temp1) {
+					PATH.add(s);
+				}
+				store.addItem(new Item(itemID, maxStock, realStock, itemName, PATH), PATH);
 				
 				// Add the item to an aisle here
 				
